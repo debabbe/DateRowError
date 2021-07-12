0885759702d51e180a2e8c45c54200a943df627c
Weber <MWe@insiders.zz>
2021-07-12 11:19:13 +0200

    Solution for reproducing DataRow error

---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("DataRowError")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Insiders Technologies")]
+[assembly: AssemblyProduct("DataRowError")]
+[assembly: AssemblyCopyright("Copyright © Insiders Technologies 2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("d6592996-a378-4994-8302-69650d9bc705")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ UnitTest1.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace DataRowError
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [DataRow(true)]
+        [DataRow(false)]
+        [DataTestMethod]
+        public void Test(bool boolean)
+        {
+            Assert.AreEqual(boolean, boolean);
+        }
+    }
+}
